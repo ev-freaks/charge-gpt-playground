90c82368022c8ce811161839197f5543a85bc4cb
Remus Lazar <remus@lazar.info>
2023-07-20 14:05:15 +0200

    Cleanup Code

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import ChargeGPT from './charge-gpt.js'
 import dotenv from 'dotenv'
 
-dotenv.config();
-
-const chargeGPT = new ChargeGPT(process.env.API_KEY);
-
-const text = 'I want to charge near Alexanderplatz, Berlin at 3pm. I need DC charger.';
-
 const main = async () => {
+  const chargeGPT = new ChargeGPT(process.env.API_KEY);
+  const text = 'I want to charge near Alexanderplatz, Berlin at 3pm. I need DC charger.';
+
   const result = await chargeGPT.request(text);
   console.log(result);
 }
+
+dotenv.config();
 
 main().then(() => {
   process.exit(0);
