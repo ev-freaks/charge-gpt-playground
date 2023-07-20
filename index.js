bd6f8fcb80255994551b84ce7623545cc49ebc15
Remus Lazar <remus@lazar.info>
2023-07-20 13:58:54 +0200

    Rework the logic in index.js to use await/async

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ dotenv.config();
 
 const chargeGPT = new ChargeGPT(process.env.API_KEY);
 
-
 const text = 'I want to charge near Alexanderplatz, Berlin at 3pm. I need DC charger.';
 
-chargeGPT.request(text)
-  .then(result => {
-    console.log(result);
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(`ERRPR: ${err.message}`);
-    console.error(err);
-    process.exit(1);
-  });
+const main = async () => {
+  const result = await chargeGPT.request(text);
+  console.log(result);
+}
+
+main().then(() => {
+  process.exit(0);
+}).catch(err => {
+  console.error(`ERROR: ${err.message}`);
+  console.error(err);
+  process.exit(1);
+});
